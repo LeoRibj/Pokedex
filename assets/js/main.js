4f64e0b1fd8b8272fa32db5e43f4db1570ea9608
ManigoldoCR <leo_dbz1@hotmail.com>
2023-08-11 11:21:51 -0300

    Primeiro push

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,31 @@
 offset = 0
 limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) =>jsonBody.results)
-    .then((pokemonList) => {
-        debugger 
-        console.log(pokemonList)})
+function converthtml(pokemon){
+    return `<li class="pokemon">
+    <span class="number">#001</span>
+    <span class="name">${pokemon.name}</span>
+<div class="detail">
+    <ol class="types">
+        <li class="type">grass</li>
+        <li class="type">poison</li>
+
+    </ol>
+
+
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+    alt=${pokemon.name}>
+
+</div>  
     
+</li>`
+}
 
-    .catch() 
+
+const pokemonOL = document.getElementById('pokemonList')
+ 
+pokeApi.getpokemons().then((pokemonList =[])=>{ 
+    pokemonOL.innerHTML += pokemonList.map((converthtml)).join('')
+
+})
