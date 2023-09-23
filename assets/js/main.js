8e42c33affe47b68fe92186ded66f0d3d811d1cc
ManigoldoCR <leo_dbz1@hotmail.com>
2023-09-23 11:53:05 -0300

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,15 @@ const maxRecord = 151
 
 const pokemonOL = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadmore')
+
 function loadpokemonItens(offset,limit){
     pokeApi.getpokemons(offset,limit).then((pokemonList =[])=>{ 
+        
         const newHtml = pokemonList.map((pokemon)=>`
-            <li class="pokemon ${pokemon.type}">
+        <a href="page2.html"> <li class="pokemon ${pokemon.type}">
+                
                 <span class="number">#${pokemon.number}</span>
-                <span class="name">${pokemon.name}</span>
+                <span class=>${pokemon.name}</span>
                 <div class="detail">
                     <ol class="types">
                         ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
@@ -24,9 +27,12 @@ function loadpokemonItens(offset,limit){
         
             </div>  
             
-            </li>   
+            </li> 
+        </a>
          `).join(' ')
-        pokemonOL.innerHTML += newHtml
+        pokemonOL.innerHTML += newHtml   
+        
+        
     })
 } 
 
@@ -45,4 +51,3 @@ loadMoreButton.addEventListener('click', () => {
         loadpokemonItens(offset, limit)
     }
     })
-
